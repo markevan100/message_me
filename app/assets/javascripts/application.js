1dfd891fdadfbfeae246cadf79c1cfeab4934237
Mark Modak <mmodak@mmodak.local>
2019-03-06 13:01:11 -0700

    navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
